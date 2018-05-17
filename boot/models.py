e94703ac3b835503b9cc7e2c0b84ac7f4080dd0a
hurricane1260 <hurricane1260@gmail.com>
2018-05-17 17:23:27 +0800

    modify time_zone & language_code in settings.py

--- boot/models.py
+++ boot/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    """
+    菜单
+    """
+    menu_id = models.IntegerField(unique=True,)
+    title = models.CharField(max_length=32, unique=True)
+    parent = models.ForeignKey("Menu", null=True, blank=True)
+    # 定义菜单间的自引用关系
+    # 权限url 在 菜单下；菜单可以有父级菜单；还要支持用户创建菜单，因此需要定义parent字段（parent_id）
+    # blank=True 意味着在后台管理中填写可以为空，根菜单没有父级菜单
+
+    def __str__(self):
+        # 显示层级菜单
+        title_list = [self.title]
+        p = self.parent
+        while p:
+            title_list.insert(0, p.title)
+            p = p.parent
+        return '-'.join(title_list)
