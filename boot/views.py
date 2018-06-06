547a1f0e0470fd1cb4941cce7cbd7e6f617bca70
liushuo <liushuo@jzsec.com>
2018-06-06 14:15:37 +0800

    update error code

--- boot/views.py
+++ boot/views.py
@@ -20,27 +20,27 @@ def boot_login(request):
     req = json.loads(request.body)
 
     if 'type' not in req:
-        resp_content['code'] = '3'
+        resp_content['code'] = '8001'
         resp_content['msg'] = "missing login type"
         return Response(resp_content)
     type = req['type']
 
     if 'userName' not in req:
-        resp_content['code'] = '3'
+        resp_content['code'] = '8001'
         resp_content['msg'] = "missing login userName"
         return Response(resp_content)
     username = req['userName']
 
     if operator.eq(type, 'account') :
         if 'password' not in req:
-            resp_content['code'] = '3'
+            resp_content['code'] = '8001'
             resp_content['msg'] = "missing login password"
             return Response(resp_content)
         password = req['password']
         user = authenticate(username=username, password=password)
     elif operator.eq(type, 'mobile') :
         if 'captcha' not in req:
-            resp_content['code'] = '3'
+            resp_content['code'] = '8001'
             resp_content['msg'] = "missing captcha"
             return Response(resp_content)
         captcha = req['captcha']
