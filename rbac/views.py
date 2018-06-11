af40bdb4853db7890c1de0d27a541d6b85e21586
hurricane1260 <hurricane1260@gmail.com>
2018-06-11 17:15:45 +0800

    add rbac models

--- rbac/views.py
+++ rbac/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+
+
+@api_view(['POST'])
+@csrf_exempt
+def create_user(request):
+    '''
+    create new user
+    :param request:
+    :return:
+    '''
+    # TODO:
+    return
+
+@api_view(['GET'])
+def users(request):
+    '''
+    get users list
+    :param request:
+    :return:
+    '''
+    # TODO:
+    return
+
+@api_view(['POST'])
+def delete_user(request):
+    '''
+    delete user,actually,wo don't delete user phyicaly,wo mark the user's status to be frozen
+    :param request:
+    :return:
+    '''
+    # TODO:
+    return
