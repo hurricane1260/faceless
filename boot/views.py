c8d17e45986120b7590c0de96801a62a4b71f31e
hurricane1260 <hurricane1260@gmail.com>
2018-06-12 15:08:30 +0800

    add token

--- boot/views.py
+++ boot/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 import json
 import operator
+
+
+def auth(func):
+    '''判断是否登录装饰器'''
+
+    def inner(request, *args, **kwargs):
+        ck = request.session.get("user_id")
+        if not ck:
+            return redirect("/login.html")
+        return func(request, *args, **kwargs)
+
+    return inner
 
 @api_view(['POST'])
 @csrf_exempt
@@ -55,11 +68,13 @@ def boot_login(request):
         resp_content['code'] = '10001'
         resp_content['msg'] = "user not invalid"
     else:
+        token = Token.objects.get(user=user)
         group = user.groups.first()
         resp_content['type'] = type
         resp_content['user'] = {
             'userId':user.id,
             'userName':user.username,
-            'userGroup':group.name
+            'userGroup':group.name,
+            'token':token
         }
     return Response(resp_content)
